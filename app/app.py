123bdbd14be4e56196167ac66607381a75fe2a7b
venugopal <venugopal.reddy@onsite.co.in>
2021-05-22 15:23:42 +0530

    validations added for signup

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from logging import error
+from mysql.connector import errors
+
+from traitlets.traitlets import validate
 import settings
 import mysql.connector as mysqldb
 from random import randint
@@ -6,6 +10,7 @@ import json
 import hashlib
 import binascii
 import os
+import re
 
 
 mysqldb_connection = mysqldb.connect(user=settings.DB_USER, password='Gopalvenu@113',
@@ -49,17 +54,33 @@ class GetHandler(SimpleHTTPRequestHandler):
             return 'Missing fields - %s ' % (', '.join(missing_fields)), 400
         username = post_data.get('username', None)
         password = post_data.get('password', None)
-        password = self.hash_password(password)
         email = post_data.get('email', None)
         phone = post_data.get('phone', None)
 
-        # add conditions
-        cur = mysqldb_connection.cursor()
-        cur.execute('INSERT INTO login (username, password, email, phone) VALUES ("%s", "%s", "%s", "%s")' % (
-            username, password, email, phone))
-        mysqldb_connection.commit()
-        cur.close()
-        return "User Created Successfully", 200
+        validation_errors = self.validate_signup_data(username, password, email, phone)
+
+        if not validation_errors:
+            password = self.hash_password(password)
+            cur = mysqldb_connection.cursor()
+            cur.execute('INSERT INTO login (username, password, email, phone) VALUES ("%s", "%s", "%s", "%s")' % (
+                username, password, email, phone))
+            mysqldb_connection.commit()
+            cur.close()
+            return "User Created Successfully", 200
+        else:
+            return str(validation_errors), 400
+    
+    def validate_signup(username, password, email, phone):
+        errors = []
+        if '@' not in email:
+            errors.append('Not a valid email')
+        if len(username) > 8:
+            errors.append('Length of username must not exceed 8 characters')
+        if not re.match("^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{1,6}$", password):
+            errors.append('Password should contain one letter, one number, any of these [_, -, #] symbols, and must not exceed 6 characters')
+        if not re.match("^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$", phone):
+            errors.append('Not a valid phone number')
+        return ", ".join(errors)
     
     def login(self, post_data):
         required_fields = ['username', 'password']
